7d412612d15c2bc324463bec3350c43b6b280eb7
Gursimranpreet Kaur <Gursimranpreetkaur@georgebrown.cagit config --global user.email Gursimranpreetkaur@georgebrown.ca>
2026-02-19 15:35:27 -0500

    Student Name updated

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student ID: 101575906</h1>
-      <h1>Student ID: Put Your Student Name</h1>
+      <h1>Student Name: Gursimranpreet Kaur</h1>
       <h1>Student Information System</h1>
       <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
       <StudentForm addStudent={addStudent} />
